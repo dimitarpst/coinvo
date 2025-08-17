d41ad5e7480bed4341b37f5fca7ecd0c66f2bdd2
dist <poketimbg@gmail.com>
2025-08-17 19:35:56 +0300

    Add PrismaService/Module and wire into backend AppModule

--- apps/backend/src/app.module.ts
+++ apps/backend/src/app.module.ts
@@ -1,8 +1,13 @@
 import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
 import { ParseModule } from './parse/parse.module';
+import { PrismaModule } from './prisma/prisma.module';
 
 @Module({
-  imports: [ConfigModule.forRoot({ isGlobal: true }), ParseModule],
+  imports: [
+    ConfigModule.forRoot({ isGlobal: true }),
+    PrismaModule,
+    ParseModule,
+  ],
 })
 export class AppModule {}
